66d2d2bac992692ba97f976a4caacfb8b9dfdc4b
Evgeny Chernyak <evgen.mosty@gmail.com>
2019-06-13 22:32:41 +0300

    add option to restart docker container on failures

--- deployment/build_and_run.sh
+++ deployment/build_and_run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker build --tag drf .
-docker run -p 5010:8000 --name drf_container drf > logs.txt 2>&1 &
+docker run -p 5010:8000
+    --restart always
+    --name drf_container drf > logs.txt 2>&1 &
